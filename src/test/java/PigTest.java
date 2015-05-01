21e9215676a97ee6136eea2285282459bef7f0a2
krishna <krishnakalyan3@gmail.com>
2015-05-02 03:13:37 +0530

    .

--- src/test/java/PigTest.java
+++ src/test/java/PigTest.java
@@ -7,19 +7,19 @@ import java.io.IOException;
 /**
  * Created by krishna on 02/05/15.
  */
+
 public class PigTest {
 
     public static void main(String[] args) throws IOException {
 
-        PigServer pigServer = new PigServer(ExecType.MAPREDUCE);
-        runMyQuery(pigServer, "input");
+        PigServer pigServer = new PigServer(ExecType.LOCAL);
+        runMyQuery(pigServer, "src/resource/");
     }
 
 
     public static void runMyQuery(PigServer pigServer, String inputFile) throws IOException {
+
         pigServer.registerQuery("A = load '" + inputFile + "' using PigStorage();");
-
-
         pigServer.store("A", "myoutput");
     }
 
